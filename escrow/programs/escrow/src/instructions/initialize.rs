63cdb59013fed54cba69377f56a4545237d97df7
fyusuf-a <florian.yusuf-ali@ashiba.dev>
2024-10-27 12:43:24 +0100

    feat(escrow): creates the contract

--- escrow/programs/escrow/src/instructions/initialize.rs
+++ escrow/programs/escrow/src/instructions/initialize.rs
@@ -1,4 +1,34 @@
-use anchor_lang::Accounts;
+use anchor_lang::prelude::*;
 
 #[derive(Accounts)]
-pub struct Initialize {}
+pub struct Initialize<'info> {
+    #[account(mut)]
+    pub payer: Signer<'info>,
+    #[account()]
+    pub payee: UncheckedAccount<'info>,
+    #[account(
+        init,
+        payer = payer,
+        seeds = [b"state", payer.key().as_ref(), payee.key().as_ref()],
+        bump,
+        space = VaultState::INIT_SPACE,
+    )]
+    pub vault_state: Account<'info, VaultState>,
+    pub system_program: Program<'info, System>
+}
+
+#[account]
+pub struct VaultState {
+    pub state_bump: u8,
+}
+
+impl Space for VaultState {
+    const INIT_SPACE: usize = 8 + 1;
+}
+
+impl<'info> Initialize<'info> {
+    pub fn initialize(&mut self, bumps: &InitializeBumps) -> Result<()> {
+        self.vault_state.state_bump = bumps.vault_state;
+        Ok(())
+    }
+}
