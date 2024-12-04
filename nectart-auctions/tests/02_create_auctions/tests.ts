a4c81f34f82222e55c42c6236aa3cb01481dd212
fyusuf-a <florian.yusuf-ali@ashiba.dev>
2024-12-04 19:07:56 +0100

    test(nectart-auctions): creates an auction with an auctioneer keypair

--- nectart-auctions/tests/02_create_auctions/tests.ts
+++ nectart-auctions/tests/02_create_auctions/tests.ts
@@ -1,26 +1,26 @@
 import * as anchor from "@coral-xyz/anchor";
 import { Program } from "@coral-xyz/anchor";
 import { createUmi } from "@metaplex-foundation/umi-bundle-defaults";
-import { auctioneer } from "../nectart-auctions";
-import { createSignerFromKeypair, generateSigner, keypairIdentity, KeypairSigner } from "@metaplex-foundation/umi";
-import { findMasterEditionPda, findMetadataPda, mplTokenMetadata, verifySizedCollectionItem } from "@metaplex-foundation/mpl-token-metadata";
+import { generateSigner, signerIdentity } from "@metaplex-foundation/umi";
+import { findMasterEditionPda, findMetadataPda, mplTokenMetadata } from "@metaplex-foundation/mpl-token-metadata";
 import { mockStorage } from '@metaplex-foundation/umi-storage-mock';
 import { airdrop_if_needed, createNft, ONE_MINUTE, ONE_SECOND, } from '../lib';
 import { NectartAuctions } from "../../target/types/nectart_auctions";
 import { BN } from "bn.js";
 import { getAssociatedTokenAddressSync} from "@solana/spl-token";
 import { toWeb3JsPublicKey } from "@metaplex-foundation/umi-web3js-adapters";
-import NodeWallet from "@coral-xyz/anchor/dist/cjs/nodewallet";
+import { Keypair } from "@solana/web3.js";
 
 
 const provider = anchor.AnchorProvider.env();
 
-const payer = provider.wallet as NodeWallet;
-
 const umi = createUmi(provider.connection);
-const wallet = umi.eddsa.createKeypairFromSecretKey(new Uint8Array(payer.payer.secretKey));
-const creator = createSignerFromKeypair(umi, wallet);
-umi.use(keypairIdentity(creator));
+const auctioneer = generateSigner({ eddsa: umi.eddsa });
+const secretKey = auctioneer.secretKey;
+const web3JsSigner = Keypair.fromSecretKey(secretKey);
+
+
+umi.use(signerIdentity(auctioneer));
 umi.use(mplTokenMetadata());
 umi.use(mockStorage());
 anchor.setProvider(provider);
@@ -28,18 +28,18 @@ anchor.setProvider(provider);
 const program = anchor.workspace.NectartAuctions as Program<NectartAuctions>;
 
 it("Creates an auction", async () => {
-  await airdrop_if_needed(provider, auctioneer.publicKey, 5);
+  await airdrop_if_needed(provider, toWeb3JsPublicKey(auctioneer.publicKey), 5);
   const { collectionMint, nftMint } = await createNft(umi);
   const THIRTY_SECONDS = 30 * ONE_SECOND;
-  const mintAta = getAssociatedTokenAddressSync(toWeb3JsPublicKey(nftMint.publicKey), provider.wallet.publicKey);
+  const mintAta = getAssociatedTokenAddressSync(toWeb3JsPublicKey(nftMint.publicKey), toWeb3JsPublicKey(auctioneer.publicKey));
   const nftEdition = findMasterEditionPda(umi, { mint: nftMint.publicKey });
   const auction = anchor.web3.PublicKey.findProgramAddressSync([Buffer.from('auction'), toWeb3JsPublicKey(nftMint.publicKey).toBuffer()], program.programId)[0];
   const auctionVault = anchor.web3.PublicKey.findProgramAddressSync([Buffer.from('vault'), toWeb3JsPublicKey(nftMint.publicKey).toBuffer()], program.programId)[0];
   const nftMetadata = findMetadataPda(umi, { mint: nftMint.publicKey });
 
   await program.methods.createAuction(new BN(THIRTY_SECONDS), new BN(ONE_MINUTE), new BN(0), new BN(0))
-    .accountsPartial({
-      payer: provider.wallet.publicKey,
+    .accounts({
+      payer: auctioneer.publicKey,
       mint: nftMint.publicKey,
       collectionMint: collectionMint.publicKey,
       mintAta,
@@ -48,5 +48,6 @@ it("Creates an auction", async () => {
       auction,
       auctionVault,
     })
+    .signers([web3JsSigner])
     .rpc();
 });
